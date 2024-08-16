f6dfd0f2720eb01c04d617716ca98b36d5b92d0b
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-08-16 07:49:59 +0545

    Final program

---
+++ include/arrayinfo.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+void generateArray(int size);
+void generateArrayFromInput(const std::vector<int>& inputArray);
+void drawArray(sf::RenderWindow& window);
