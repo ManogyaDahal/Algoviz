1caf5654df58c6aaa6b66e99ad5a95c4e5e8f021
ManogyaDahal <bat16692@gmail.com>
2024-08-17 19:16:48 +0545

    fixing commits

--- include/arrayinfo.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-
-void generateArray(int size);
-void generateArrayFromInput(const std::vector<int>& inputArray);
-void drawArray(sf::RenderWindow& window);
--- include/gui.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-
-void ShowMenuBar(bool* p_open);
-void displayUserInputBox(sf::RenderWindow& window);
-void displaySwapInfoBox(sf::RenderWindow& window);
-void displayTimeComplexityBox(sf::RenderWindow& window);
